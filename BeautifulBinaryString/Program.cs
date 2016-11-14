8a795d4247e3fbe3eaf04d573c927dd0449abfdb
diogo.carvalho <diogo.carvalho@DEV02>
2016-11-13 23:17:59 -0200

    Beautiful Binary String chalenge done.

---
+++ BeautifulBinaryString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautifulBinaryString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string B = Console.ReadLine();
+
+            Console.Write((B.Length - B.Replace("010", "").Length) / 3);
+        }
+    }
+}
