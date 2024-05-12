c2eb86ec5408f8bf1e00e4b6c7be375501f61d19
Diogo <diogo.santos-carvalho@rabobank.com>
2024-05-12 11:43:38 +0200

    add new project with a new hackerrank challenge

---
+++ InifinityStringRepeatedChar/Result.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InifinityStringRepeatedChar
+{
+    class Result
+    {
+        /*
+         * Complete the 'repeatedString' function below.
+         *
+         * The function is expected to return a LONG_INTEGER.
+         * The function accepts following parameters:
+         *  1. STRING s
+         *  2. LONG_INTEGER n
+         */
+
+        public static long repeatedString(string s, long n)
+        {
+            long repeatedCharNumber = 0;
+
+            if (string.IsNullOrWhiteSpace(s)) return 0;
+
+            int stringLenght = s.Length;
+            if (stringLenght == 1)
+            {
+                return s.Equals("a", StringComparison.CurrentCultureIgnoreCase) ? n : repeatedCharNumber;
+            }
+
+            long lenghtRatio = n / stringLenght;
+            long remainingLengh = n - (lenghtRatio * stringLenght);
+
+            for (int index = 0; index < stringLenght; index++)
+            {
+                if (s[index] == 'a')
+                {
+                    repeatedCharNumber++;
+                }
+            }
+
+            repeatedCharNumber = repeatedCharNumber * lenghtRatio;
+
+            for (int index = 0; index < remainingLengh; index++)
+            {
+                if (s[index] == 'a')
+                {
+                    repeatedCharNumber++;
+                }
+            }
+
+            return repeatedCharNumber;
+
+        }
+
+    }
+}
